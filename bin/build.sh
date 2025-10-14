2793fe26debaf49ae6ff5b518e02f71fea5ff00e
JordaanI <jordaanivan@gmail.com>
2025-10-13 22:37:00 -0400

    Changed build rules and added git ignore

--- bin/build.sh
+++ bin/build.sh
@@ -1,1 +1,9 @@
-gsc -cc-options "-I/opt/homebrew/Cellar/raylib/5.5/include -L/opt/homebrew/Cellar/raylib/5.5/lib -lraylib" -exe ../lib/loader.scm
+if [ -e ../lib/loader.o1 ]; then
+    echo "Removing previous loader"
+    rm ../lib/loader.o1
+fi
+
+echo "Compiling Raybit.."
+gsc -cc-options "-I/opt/homebrew/Cellar/raylib/5.5/include -L/opt/homebrew/Cellar/raylib/5.5/lib -lraylib" ../lib/loader.scm
+mv ../lib/loader.o1 .
+echo "Done: compiled to loader.o1"
