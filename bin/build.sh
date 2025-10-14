c5a14ab0f284866e032d68ccb1680c6e068750eb
JordaanI <jordaanivan@gmail.com>
2025-10-14 09:56:49 -0400

    changed compilation options

--- bin/build.sh
+++ bin/build.sh
@@ -11,6 +11,6 @@ if [ -e "$root/lib/loader.o" ]; then
 fi
 
 echo "Compiling Raybit.."
-logExe "gsc -obj -cc-options \"-I/opt/homebrew/Cellar/raylib/5.5/include -L/opt/homebrew/Cellar/raylib/5.5/lib -lraylib\" ../lib/loader.scm"
+logExe "gsc -obj -cc-options \"-I/opt/homebrew/Cellar/raylib/5.5/include -L/opt/homebrew/Cellar/raylib/5.5/lib -lraylib -D___SHARED -D___DYNAMIC -D___LIBRARY\" ../lib/loader.scm"
 logExe "mv $root/lib/loader.o $root/bin"
 echo "Done: compiled to $root/bin/loader.o"
