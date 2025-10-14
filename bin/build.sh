34febbf7a0efe74acb38bf1cf8b1418833887ee2
JordaanI <jordaanivan@gmail.com>
2025-10-14 08:02:36 -0400

    changed o1 to o

--- bin/build.sh
+++ bin/build.sh
@@ -4,6 +4,6 @@ if [ -e ../lib/loader.o1 ]; then
 fi
 
 echo "Compiling Raybit.."
-gsc -cc-options "-I/opt/homebrew/Cellar/raylib/5.5/include -L/opt/homebrew/Cellar/raylib/5.5/lib -lraylib" ../lib/loader.scm
-mv ../lib/loader.o1 .
-echo "Done: compiled to loader.o1"
+gsc -obj -cc-options "-I/opt/homebrew/Cellar/raylib/5.5/include -L/opt/homebrew/Cellar/raylib/5.5/lib -lraylib" ../lib/loader.scm
+mv ../lib/loader.o .
+echo "Done: compiled to loader.o"
